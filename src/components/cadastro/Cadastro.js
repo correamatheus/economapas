e1135664f51a85cb5cff4faed4f479aabea261f2
unknown <matheuscorreati@gmail.com>
2022-03-03 09:42:48 -0300

    remocao de comentarios

--- src/components/cadastro/Cadastro.js
+++ src/components/cadastro/Cadastro.js
@@ -1,30 +1,24 @@
 import { useState } from "react";
 import styles from "./Cadastro.module.css";
 
-
 function Cadastro({ onSubmit }) {
   const [name, setName] = useState("");
   const [mostrarQuestoes, setMostrarQuestoes] = useState(false);
   const [selecaoDificuldade, setSelecaoDificuldade] = useState("easy");
   const dificuldade = ["easy", "hard"];
-  const [validaCampo, setValidaCampo] = useState(false)
+  const [validaCampo, setValidaCampo] = useState(false);
 
   const enviarFormulario = (e) => {
-    
-    if(name.value === 0 ){
-      setValidaCampo(false)
+    if (name.value === 0) {
+      setValidaCampo(false);
     }
-    
-    if(name.length > 0){
+
+    if (name.length > 0) {
       e.preventDefault();
-      console.log("dados componente", { name });
       onSubmit({ name, selecaoDificuldade });
       setMostrarQuestoes(true);
-      console.log("mostrar questoes component", mostrarQuestoes);
-      console.log(selecaoDificuldade);
-      setValidaCampo(true)
+      setValidaCampo(true);
     }
-   
   };
 
   return (
@@ -37,17 +31,21 @@ function Cadastro({ onSubmit }) {
           name="name"
           id="name"
           value={name}
-          onChange={(e) => setName(e.target.value)}   
-  
+          onChange={(e) => setName(e.target.value)}
         />
-               <select onChange={(e) => setSelecaoDificuldade(e.target.value)}>
+        <select onChange={(e) => setSelecaoDificuldade(e.target.value)}>
           {dificuldade.map((item) => (
             <option value={item}>{item}</option>
           ))}
         </select>
-        {!validaCampo && <p className={styles.info}>Para iniciar o jogo preencha o campo nome - Campo obrigatório</p>}
-        <button type="submit" className={styles.button_style}>Começar a jogar</button>
- 
+        {!validaCampo && (
+          <p className={styles.info}>
+            Para iniciar o jogo preencha o campo nome - Campo obrigatório
+          </p>
+        )}
+        <button type="submit" className={styles.button_style}>
+          Começar a jogar
+        </button>
       </form>
     </>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,15 +7,12 @@ import Questions from "../questions/Questions";
 function Home() {
   const [name, setName] = useState();
   const [mostrarQuestoes, setMostrarQuestoes] = useState(false);
-  const [selecaoDificuldade, setSelecaoDificuldade] = useState('')
+  const [selecaoDificuldade, setSelecaoDificuldade] = useState("");
 
   const handleSubmit = ({ name, selecaoDificuldade }) => {
-    console.log("dados home", { name });
     setName(name);
     setMostrarQuestoes(true);
     setSelecaoDificuldade(selecaoDificuldade);
-    console.log("mostrar questoes home", mostrarQuestoes);
-    console.log("mostrar difculdade home", selecaoDificuldade);
   };
 
   return (
@@ -35,18 +32,6 @@ function Home() {
         </section>
       )}
     </div>
-
-    // <section className={styles.home_container}>
-    //   <img
-    //     src={LogoEconomaps}
-    //     alt="Logo Economapas"
-    //     className={styles.home_logo}
-    //   />
-    //   <h1>Bem-vindo ao Quiz da Economapas</h1>
-
-    //   <Cadastro onSubmit={handleSubmit}/>
-    //   <Questions name={name}/>
-    // </section>
   );
 }
 
